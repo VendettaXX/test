0185351e091d337c0909c1ce8b8c58d9ffe5d407
VendettaXX <2869159097@qq.com>
2018-06-11 14:11:07 +0800

    add

---
+++ test60.c
@@ -0,0 +1,9 @@
+int add(int a,int b)
+{
+    return a+b;
+}
+
+int main(void)
+{
+    int x=add(5,7);
+}
---
+++ 柔性数组.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct test{
+    int a;
+    int s[];
+};
+int main(void)
+{
+    struct test a;
+    printf("%d\n",sizeof(a));
+    struct test * b=(struct test *)malloc(sizeof(struct test)+100*sizeof(int));
+    for(int i=0;i<100;i++)
+    {
+        b->s[i]=i;
+    }
+    for(int i=0;i<100;i++)
+    {
+        printf("%d\t",b->s[i]);
+    }
+    printf("%d\n",sizeof(b));
+    free(b);
+    return 0;
+}
